b4e110f57e24fd4cfeac98edc8bbb602f8dc1f7d
yusuf.sarman <yusuf.sarman@kion.com.tr>
2023-09-01 04:41:35 +0300

    first migration for report database

--- Report/Report/Infrastructure/AppDbContext.cs
+++ Report/Report/Infrastructure/AppDbContext.cs
@@ -1,16 +1,55 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ReportApi.Entities;
+using System.Reflection.Emit;
 
 namespace ReportApi.Infrastructure
 {
     public class AppDbContext:DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-       
+        public DbSet<Report> Reports { get; set; }
+        public DbSet<ReportDetail> ReportDetails { get; set; }
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
 
-          
+            builder.Entity<Report>(entity =>
+            {
+                entity.ToTable("Reports"); 
+                entity.HasKey(e => e.Id);
+
+                entity.Property(e => e.Id).IsRequired();
+                entity.Property(e => e.CreateTime).IsRequired();
+                entity.Property(e => e.CompletionTime);                
+                entity.Property(e => e.Status)
+                    .IsRequired()
+                    .HasConversion<string>();
+                // Configure the relationship with ReportDetail
+                entity.HasMany(e => e.ReportDetail)
+                    .WithOne(e => e.Report)
+                    .HasForeignKey(e => e.ReportId)
+                    .OnDelete(DeleteBehavior.Cascade);
+            });
+
+            builder.Entity<ReportDetail>(entity =>
+            {
+                entity.ToTable("ReportDetails"); 
+
+                entity.HasKey(e => e.Id);
+
+                entity.Property(e => e.Id).IsRequired();
+                entity.Property(e => e.ReportId).IsRequired();
+                entity.Property(e => e.Location);
+                entity.Property(e => e.PhoneCount);
+                entity.Property(e => e.EmailCount);
+
+                
+                entity.HasOne(e => e.Report)
+                    .WithMany(e => e.ReportDetail)
+                    .HasForeignKey(e => e.ReportId)
+                    .OnDelete(DeleteBehavior.Cascade); 
+            });
+
         }
     }
 }
---
+++ Report/Report/Migrations/20230901014057_initial.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReportApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Reports",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreateTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    CompletionTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    Status = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reports", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ReportDetails",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ReportId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Location = table.Column<string>(type: "text", nullable: false),
+                    PhoneCount = table.Column<string>(type: "text", nullable: false),
+                    EmailCount = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReportDetails", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ReportDetails_Reports_ReportId",
+                        column: x => x.ReportId,
+                        principalTable: "Reports",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReportDetails_ReportId",
+                table: "ReportDetails",
+                column: "ReportId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ReportDetails");
+
+            migrationBuilder.DropTable(
+                name: "Reports");
+        }
+    }
+}
